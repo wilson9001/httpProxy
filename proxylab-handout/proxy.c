ef85d8ea819f9b5245ce4e020cbde023da306786
Alex Wilson <wilsonalexanderjohn@byu.net>
2018-03-18 01:54:53 -0600

    Finished initial code. Will look over it and begin testing next...

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -9,7 +9,7 @@
 #define MAX_OBJECT_SIZE 102400
 #define BUFMAX 1000
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr = "\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
 static const char *connectionHeader = "Connection: close";
 static const char *proxyHeader = "Proxy-Connection: close";
@@ -68,9 +68,9 @@ void *thread(void *vargp)
     Rio_readinitb(&rioClient, connfd);
     n = Rio_readlineb(&rioClient, buf, BUFMAX);
 
-    //TODO: parse request to get address, port, URI, and headers.
+    //parse request to get address, port, URI, and headers.
     char *hostname;
-    char *port;
+    char *portNumber = NULL;
     char *reqWire;
 
     char headerTestSubstr[6];
@@ -79,28 +79,38 @@ void *thread(void *vargp)
 
     strupr(headerTestSubstr);
 
-    if(!strcmp(headerTestSubstr, "GET"))
+    if (!strcmp(headerTestSubstr, "GET"))
     {
-        //TODO: Generate bad method response to client
+        //generate bad method response to client
+        char *responseMessage = "HTTP/1.0 405 Method Not Allowed\r\n\r\n";
 
+        Rio_writen(connfd, responseMessage, sizeof(responseMessage));
 
+        Close(connfd);
 
+        return NULL;
     }
 
-    unsigned int reqBufPos = 4; 
+    unsigned int reqBufPos = 4;
 
     memcpy(headerTestSubstr, &buf[reqBufPos], 5);
     headerTestSubstr[5] = '\0';
 
-    if(strcmp(headerTestSubstr, "http:") || strcmp(headerTestSubstr, "https"))
+    if (strcmp(headerTestSubstr, "http:") || strcmp(headerTestSubstr, "https"))
     {
-        //TODO: generate malformed url response to client
+        //generate malformed request response to client
+        char *responseMessage = "HTTP/1.0 400 Bad Request\r\n\r\n";
 
+        Rio_writen(connfd, responseMessage, sizeof(responseMessage));
 
+        Close(connfd);
+
+        return NULL;
     }
+
     reqBufPos += 7;
 
-    if(strcmp(headerTestSubstr, "https"))
+    if (strcmp(headerTestSubstr, "https"))
     {
         reqBufPos++;
     }
@@ -109,9 +119,9 @@ void *thread(void *vargp)
     unsigned int portSpot = 0;
     unsigned int baseURLWithPortLength = 0;
 
-    while(buf[reqBufPos + baseURLlength] != '/')
+    while (buf[reqBufPos + baseURLlength] != '/')
     {
-        if(buf[reqBufPos + baseURLlength] == ':')
+        if (buf[reqBufPos + baseURLlength] == ':')
         {
             portSpot = reqBufPos + baseURLlength;
         }
@@ -121,20 +131,19 @@ void *thread(void *vargp)
 
     baseURLWithPortLength = baseURLlength;
 
-    char *portNumber = NULL;
     int portLength = 0;
 
-    if(portSpot)
+    if (portSpot)
     {
         portLength = (reqBufPos + baseURLlength) - (portSpot /*+1*/);
 
         char portN[portLength /*+ 1*/];
-        memcpy(portN, &buf[portSpot +1], portLength);
+        memcpy(portN, &buf[portSpot + 1], portLength);
         portN[portLength] = '\0';
 
         portNumber = portN;
 
-        baseURLlength -= (portLength +1);
+        baseURLlength -= (portLength + 1);
     }
 
     char baseURL[baseURLlength + 1];
@@ -146,30 +155,81 @@ void *thread(void *vargp)
 
     size_t URIlength = 0;
 
-    while(buf[reqBufPos] != '\r' && buf[reqBufPos + 1] != '\n')
+    while (buf[reqBufPos + URIlength] != '\r' && buf[reqBufPos + URIlength + 1] != '\n')
     {
         URIlength++;
     }
 
-    char URI[URIlength +1];
+    char URI[URIlength + 1];
 
     memcpy(URI, &buf[reqBufPos], URIlength);
 
-    URI[URIlength -1] = '0';
+    URI[URIlength - 1] = '0';
     URI[URIlength] = '\0';
 
-    //extract headers
-    
+    reqBufPos += URIlength;
+
+    //copy headers
+    char passThroughHeaders[2000];
+
+    memset(passThroughHeaders, '\0', sizeof(passThroughHeaders));
+    strcpy(passThroughHeaders, &buf[reqBufPos]);
+
+    //determine if required headers are already included
+    char *hostHeaderBegin = strstr(passThroughHeaders, "Host:");
+    char *connectHeaderBegin = strstr(passThroughHeaders, "Connection:");
+    char *proxyConnectHeaderBegin = strstr(passThroughHeaders, "Proxy-Connection:");
+    char *userAgentHeaderBegin = strstr(passThroughHeaders, "User-Agent:");
+
+    //begin assembling request string for server endpoint
+    reqWire = "GET /";
+
+    strcat(reqWire, URI);
+
+    if (!hostHeaderBegin)
+    {
+        char *hostHeader = "Host: ";
+        strcat(hostHeader, baseURL);
+        strcat(reqWire, hostHeader);
+    }
+
+    if (!connectHeaderBegin)
+    {
+        strcat(reqWire, "\r\nConnection: close");
+    }
+
+    if (!proxyConnectHeaderBegin)
+    {
+        strcat(reqWire, "\r\nProxy-Connection: close");
+    }
+
+    if (!userAgentHeaderBegin)
+    {
+        strcat(reqWire, user_agent_hdr);
+    }
+
+    strcat(reqWire, passThroughHeaders);
+
+    hostname = baseURL;
+
+    if (!portNumber)
+    {
+        portNumber = "80";
+    }
 
     //Connect to server
-    int serverfd = open_clientfd(hostname, port);
+    int serverfd = open_clientfd(hostname, portNumber);
 
-    if(serverfd == -1)
+    if (serverfd == -1)
     {
         //fprintf(stderr, "Failed to connect to host %s:%s\n", hostname, port);
         //TODO: generate not found response for client
 
+        char *responseMessage = "HTTP/1.0 404 Not Found\r\n\r\n";
 
+        Rio_writen(connfd, responseMessage, sizeof(responseMessage));
+
+        Close(connfd);
 
         return NULL;
     }
@@ -178,12 +238,12 @@ void *thread(void *vargp)
     Rio_writen(serverfd, reqWire, strlen(reqWire));
 
     //Read response and copy to client
-    
+
     char buf2[BUFMAX];
     rio_t rioServer;
 
     Rio_readinitb(&rioServer, serverfd);
-    while((n = Rio_readlineb(&rioServer, buf2, BUFMAX)) != 0)
+    while ((n = Rio_readlineb(&rioServer, buf2, BUFMAX)) != 0)
     {
         Rio_writen(connfd, buf2, n);
     }
